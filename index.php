6d441a9b24d63caaf27b3b5e5d777edebd05e87e
amir hossein habibi <habibi.dev@gmail.com>
2020-08-31 20:39:02 +0430

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * Plugin Name: BlueOcean woo order map
- * Plugin URI: https://github.com/Blue-Ocean-Plus/blueocean-woo-order-map
+ * Plugin URI: https://wordpress.org/plugins/blueocean-woo-order-map
  * Description: BlueOcean woocommerce order map
  * Version: 1.0
  * Author: blueocean.plus
  * Author URI: http://blueocean.plus
  * Text Domain: bo_woo_order_map
  * Domain Path: /languages
- * License: MIT
+ * License: GNU v2
  * Requires PHP: 5.6
  **/
 if (!defined('ABSPATH')) exit; // No direct access allowed
