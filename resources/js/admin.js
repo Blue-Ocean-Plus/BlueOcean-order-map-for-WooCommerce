f650645612c9ab75b6fc704c3ea88c31ee38dad1
amir hossein habibi <habibi.dev@gmail.com>
2020-09-01 21:17:23 +0430

    Update admin.js

--- resources/js/admin.js
+++ resources/js/admin.js
@@ -1,8 +1,10 @@
-document.querySelector('.updated.blue-ocean a.insights-data-we-collect').addEventListener('click', e => {
-    let d = document.querySelector('.updated.blue-ocean .description');
-    if (d.style.display === 'none') {
-        d.style.display = 'flex';
-    } else {
-        d.style.display = 'none';
-    }
+jQuery(document).ready(function () {
+    document.querySelector('.updated.blue-ocean a.insights-data-we-collect').addEventListener('click', e => {
+        let d = document.querySelector('.updated.blue-ocean .description');
+        if (d.style.display === 'none') {
+            d.style.display = 'flex';
+        } else {
+            d.style.display = 'none';
+        }
+    });
 });
