70ec7d5507dde054de3e9967f97580e601517cc3
amir hossein habibi <habibi.dev@gmail.com>
2020-09-01 21:15:20 +0430

    Update admin.js

---
+++ resources/js/admin.js
@@ -0,0 +1,8 @@
+document.querySelector('.updated.blue-ocean a.insights-data-we-collect').addEventListener('click', e => {
+    let d = document.querySelector('.updated.blue-ocean .description');
+    if (d.style.display === 'none') {
+        d.style.display = 'flex';
+    } else {
+        d.style.display = 'none';
+    }
+});
